d4cfb110b03f721f6bdbdba225f00052a9766ee2
alban-mrt <trousseau@peda-steursule.org>
2026-01-08 23:00:30 +0100

    information code

--- logique.py
+++ logique.py
@@ -7,15 +7,15 @@ def init_grille():
     """
     Crée une grille 4x4 initialisée avec des zéros.
     """
-    return [[0] * TAILLE for _ in range(TAILLE)]
+    return [[0] * TAILLE for _ in range(TAILLE)]# creer une liste de 4 liste
 
 
 def tasser_ligne(ligne):
     """
     Déplace les nombres non nuls d’une ligne vers la gauche.
     """
-    non_zeros = [x for x in ligne if x != 0]
-    return non_zeros + [0] * (TAILLE - len(non_zeros))
+    non_zeros = [x for x in ligne if x != 0]# creer une liste contenant seulement les nombre différent de zéro
+    return non_zeros + [0] * (TAILLE - len(non_zeros))# ajoute des zeros vers la droite pour que la ligne fassent toujours 4
 
 
 def fusion_ligne(ligne):
@@ -27,11 +27,11 @@ def fusion_ligne(ligne):
     i = 0
 
     while i < TAILLE - 1:
-        if ligne[i] != 0 and ligne[i] == ligne[i + 1]:
+        if ligne[i] != 0 and ligne[i] == ligne[i + 1]:# verifie que la tuile n'est pas vide et qu'elle est identique à celle de droite
             ligne[i] *= 2
             score += ligne[i]
-            ligne[i + 1] = 0
-            i += 2
+            ligne[i + 1] = 0 # supprime la tuile fusioné
+            i += 2 # on empeche une double fusion
         else:
             i += 1
 
@@ -48,8 +48,8 @@ def deplacement_gauche(grille):
     change = False
 
     for ligne in grille:
-        nl, sc = fusion_ligne(ligne[:])
-        if nl != ligne:
+        nl, sc = fusion_ligne(ligne[:])# applique une fusion sur une copie de la ligne
+        if nl != ligne: # si la ligne change alors valide
             change = True
         nouvelle.append(nl)
         score_total += sc
@@ -122,12 +122,12 @@ def ajout_chiffre(grille):
     """
     Ajoute une tuile (2 ou 4) dans une case vide de la grille.
     """
-    vides = [(i, j) for i in range(TAILLE) for j in range(TAILLE) if grille[i][j] == 0]
+    vides = [(i, j) for i in range(TAILLE) for j in range(TAILLE) if grille[i][j] == 0]# liste de toutes les positions vides
     if not vides:
         return grille
 
     i, j = random.choice(vides)
-    grille[i][j] = 4 if random.random() < 0.10 else 2
+    grille[i][j] = 4 if random.random() < 0.10 else 2# taux d'apparations d'un 2 ou un 4
     return grille
 
 
@@ -146,8 +146,8 @@ def game_over(grille):
         for j in range(TAILLE):
             if grille[i][j] == 0:
                 return False
-            if j < TAILLE - 1 and grille[i][j] == grille[i][j + 1]:
+            if j < TAILLE - 1 and grille[i][j] == grille[i][j + 1]:# verification fusion verticable possible
                 return False
-            if i < TAILLE - 1 and grille[i][j] == grille[i + 1][j]:
+            if i < TAILLE - 1 and grille[i][j] == grille[i + 1][j]:# verifications fusion horizontable possible
                 return False
     return True
